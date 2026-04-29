d651534c12e12f869b2b33ee1a5f5c413a9e9288
Regina Sun <xs2316@nyu.edu>
2026-04-29 13:30:17 +0800

    Update train_yolo.py

--- scripts/train_yolo.py
+++ scripts/train_yolo.py
@@ -2,22 +2,23 @@ from ultralytics import YOLO
 
 
 def main():
-    model = YOLO("yolov8n.pt")  # 先从最小模型开始
+    model = YOLO("C:\Users\Regina Sun\Documents\GitHub\plankton-detection\runs\yolov8n_microbe_1x_merged_retrain")
 
     model.train(
-        data=r"C:\Users\Regina Sun\Documents\GitHub\plankton-detection\data\yolo_dataset1x\data.yaml",
+        data=r"C:\Users\Regina Sun\Documents\GitHub\plankton-detection\data\yolo_dataset1x+0.5x\data.yaml",
         epochs=100,
         imgsz=640,
-        batch=16,
+        batch=16, 
         workers=4,
         device=0,
         project=r"C:\Users\Regina Sun\Documents\GitHub\plankton-detection\runs",
-        name="yolov8n_microbe_1x",
+        name="yolov8n_microbe_1x+0.5x_retrain",
         pretrained=True,
         patience=20,
-        save=True
+        save=True,
     )
 
 
+
 if __name__ == "__main__":
-    main()
+    main()   
